f54eb271c18b7ab6fc2f71267860cb8c8ae9be82
Shigeru Ichinoki <a1505si@aiit.ac.jp>
2015-06-13 01:44:41 +0900

    test 14

--- test.bash
+++ test.bash
@@ -9,20 +9,12 @@ ERROR_EXIT () {
 
 # TEST1 
 
-echo 10 > $tmp-ans
+echo 10 NG > $tmp-ans
 ./15745105 20 90 > $tmp-out || ERROR_EXIT "TEST1-1" 
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
 
 echo OK
 
-# TEST2 
-
-echo 0 > $tmp-ans
-./15745105 0 0 > $tmp-out || ERROR_EXIT "TEST1-1" 
-diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
-
-
-echo OK
 
 rm -f $tmp-*
 
